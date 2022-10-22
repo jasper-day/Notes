d94253f1fe380a8b6f2ac955099948882d49ff82
Jasper Day <jromanoday@gmail.com>
2022-10-22 23:44:26 +0100

    fm2

---
+++ Class_Notes/2A_Prog_Skill/toucan_crossing.py
@@ -0,0 +1,10 @@
+# ++============================================++
+# || Programming for Engineers: Toucan Crossing ||
+# ||-------------+------------+-----------------||
+# || Jasper Day  |  S2265891  |  2022/10/20     ||
+# ++============================================++
+
+import numpy as np
+
+class TrafficObject:
+     pos2 = np.array([])
